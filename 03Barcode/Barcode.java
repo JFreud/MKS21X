a147326c577635dccace7342219ac1afe0733d78
JFreud <jrm.frdnbrg@gmail.com>
2016-12-09 03:25:14 -0500

    Fixed Barcode to work; Still needs toZip

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,28 +1,27 @@
 public class Barcode implements Comparable<Barcode>{
-// instance variables
    private String _zip;
    private int _checkDigit;
-    private String barcoded;
     // private String[] coded = new String[] {":::||", "::|:|" , "::||:",  ":|::|"," :|:|:", ":||::"," |:::|","|::|:", "|:|::", "||:::"};
     // private int[] reference  = new int[] {1,2,3,4,5,6,7,8,9,0};
     
-    
-   
-   
-    
 
-
-// constructors
-//precondtion: _zip.length() = 5 and zip contains only digits.
-//postcondition: throws a runtime exception zip is not the correct length
-//               or zip contains a non digit
-//               _zip and _checkDigit are initialized.
   public Barcode(String zip) {
       if (!(zip.length() == 5)){//addcheck
 	  throw new IllegalArgumentException();
       }
       _zip = zip;
-      for (char c : _zip.toCharArray()){
+      toCode();
+  }
+
+
+  public Barcode clone(){
+      Barcode copy = new Barcode(_zip);
+      return copy;
+}
+
+  public String toCode() {
+      String barcoded = "";
+      for (char c : (_zip + checkSum() % 10).toCharArray()){
 	  //barcoded += coded[reference.indexOf(c - '0')];
 	  switch (c) {
 	  case '1' : barcoded += ":::||";
@@ -48,14 +47,8 @@ public class Barcode implements Comparable<Barcode>{
 	  default: throw new IllegalArgumentException();
 	  }
       }
+       return "|" + barcoded + "|";
   }
-
-// postcondition: Creates a copy of a bar code.
-  public Barcode clone(){
-      Barcode copy = new Barcode(_zip);
-      return copy;
-}
-
 
     public String getWhole() {
       _checkDigit = checkSum() % 10;
@@ -63,22 +56,17 @@ public class Barcode implements Comparable<Barcode>{
     }
       
 
-// postcondition: computes and returns the check sum for _zip
   private int checkSum(){
       int s = 0;
       for (char c : _zip.toCharArray()){
 	  s += c - '0';}
       return s;
 }
-
-//postcondition: format zip + check digit + Barcode 
-//ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
+ 
   public String toString(){
-      return _zip + " " + Integer.toString(checkSum() % 10) + " " + barcoded;
+      return _zip + " " + Integer.toString(checkSum() % 10) + " " + toCode();
   }
 
-
-// postcondition: compares the zip + checkdigit, in numerical order. 
   public int compareTo(Barcode other){
       _checkDigit = checkSum() % 10;
       String sum = getWhole();
---
+++ 03Barcode/Driver.java
@@ -0,0 +1,6 @@
+public class Driver {
+    public static void main(String[] args) {
+	Barcode tester = new Barcode("08451");
+	System.out.println(tester);
+    }
+}

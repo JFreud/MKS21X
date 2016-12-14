c05b3d3c4d035885b9eac3c3a0e0b89998425c2e
JFreud <jrm.frdnbrg@gmail.com>
2016-12-14 03:31:30 -0500

    insertionSort -- not working

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -1,7 +1,7 @@
 public class Sorts{
 
   public static String name(){
-    return "01.Freudenberg.Jerome"; 
+    return "06.Freudenberg.Jerome"; 
   }
 
 
@@ -22,9 +22,26 @@ public class Sorts{
       }
   }
 
+    public static void insertionSort(int[] data) {
+	for (int i = 1; i < data.length; i++) {
+	    int placeholder = data[i];
+	    for (int j = i - 1; j >= 0; j--) {
+		if (placeholder < data[j]) {
+		    //System.out.println(j);
+		    data[j+1] = data[j];
+		}
+		/*	else {
+		    data[j] = placeholder;
+		    }*/
+	    }
+	}
+    }
+
+		    
+
     public static void main (String[] args) {
 	int[] data = {1,63,12,5,1235,12,-7};
-	selectionSort(data);
+	insertionSort(data);
 	for(int i : data) {
 	    System.out.println(i);
 	}
